5ad6bdab7b55c24465d1e9071755f9b22b55bafd
sQKhanhs <innespham@gmail.com>
2023-04-03 15:17:14 +0700

    done

--- src/RectangleArea.java
+++ src/RectangleArea.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RectangleArea {
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter width:");
+        width = input.nextFloat();
+
+        System.out.println("Enter height:");
+        height = input.nextFloat();
+
+        float area = width * height;
+        System.out.println("Rectangle area is: " + area);
+    }
+
 }
