15f56ee86fd7672036cb3f32e257c5224ad2039c
sQKhanhs <innespham@gmail.com>
2023-04-06 16:03:42 +0700

    done

--- src/CountChar.java
+++ src/CountChar.java
@@ -5,7 +5,15 @@ public class CountChar {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter text:");
         String text = input.nextLine();
+        text = text.toLowerCase();
         System.out.println("Which letter you want to count?");
-        String letter = input.nextLine();
+        char letter = input.next().charAt(0);
+        int count = 0;
+        for(int i = 0; i < text.length(); i++){
+            if(text.charAt(i) == letter){
+                count++;
+            }
+        }
+        System.out.println("Letter counted: " + count);
     }
 }
