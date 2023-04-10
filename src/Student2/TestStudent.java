c138cc11d0af906c673876bc88629a348f1821f0
sQKhanhs <innespham@gmail.com>
2023-04-10 14:25:59 +0700

    done

---
+++ src/Student2/TestStudent.java
@@ -0,0 +1,9 @@
+package Student2;
+
+public class TestStudent {
+    public static void main(String[] args){
+        Student khanh = new Student();
+        khanh.setName("Khanh");
+        khanh.setClasses("CO3");
+    }
+}
